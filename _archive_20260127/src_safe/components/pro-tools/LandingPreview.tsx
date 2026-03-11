858f0f830941d69a7841cdf97e698f07ad94c5ec
Richard Norwood <mail@alabrida.org>
2026-03-11 18:03:01 -0400

    feat: remove Next.js project setup files

--- _archive_20260127/src_safe/components/pro-tools/LandingPreview.tsx
+++
@@ -1,9 +0,0 @@
-export function LandingPreview({ html }: { html: string }) {
-    return (
-        <iframe
-            srcDoc={html}
-            className="w-full h-full border-none"
-            title="Landing Page Preview"
-        />
-    );
-}
